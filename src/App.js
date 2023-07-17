2d301aff4b4ba33262997b21cc7a20c20f3836d5
robravi <robravi.ravi@gmail.com>
2023-07-17 22:50:25 +0530

    ist commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import EmployeeList from './component/EmployeeList.js';
+import "./EmployeeList.css";
+
+function App() {
+  return (
+    <div className="App">
+    <EmployeeList/>
+    </div>
+  );
+}
+
+export default App;
